b6c647e06d6c5fbf4d3694aa0a033448058e6b0c
Matt Green <mgreen9@gmail.com>
2012-04-16 22:21:44 -0400

    changed callback functions to static

--- StockBroker/stockbroker.h
+++ StockBroker/stockbroker.h
@@ -8,20 +8,21 @@
 
 class STOCKBROKERSHARED_EXPORT StockBroker {
 public:
-    StockBroker(void *marketOrderBuyCallback,void *marketOrderSellCallback);
+    StockBroker();
+
+    // Callback function to place a market order
+    static bool *(placeMarketBuyOrder)(QString symbol, int shares);
+    static bool *(placeMarketSellOrder)(QString symbol, int shares);
 
     void updateMarket(float value);
     void placeOrder(Order *o);
-    void executeOrder(Order *o);
 
 private:
     float m_value;
 
-    // Callback function to place a market order
-    void *placeMarketBuyOrder;
-    void *placeMarketSellOrder;
-
     QList<Order*> *orderList;
+
+    void executeOrder(Order *o);
 
 };
 
