87b1778cfecff2e69a1c48cab3ba46e5f9cec374
Ridwan Syed <ridwansi@gmail.com>
2012-04-19 12:47:07 -0400

    Added market population prototype

--- StockBroker/simulationmarket.cpp
+++ StockBroker/simulationmarket.cpp
@@ -2,4 +2,22 @@
 
 simulationMarket::simulationMarket()
 {
+
+
+}
+
+// handles updating the values from orders placed
+simulationMarket::run(){
+
+
+
+}
+
+
+// creates a fake market, with stocks, values, and quantities
+void simulationMarket::generateFakeMarketFromFile(QFile inputFile){
+
+
+
+
 }
--- StockBroker/simulationmarket.h
+++ StockBroker/simulationmarket.h
@@ -1,10 +1,31 @@
 #ifndef SIMULATIONMARKET_H
 #define SIMULATIONMARKET_H
 
-class simulationMarket
+
+#include "QThread"
+#include "QHash"
+#include "QString"
+#include "QList"
+#include "QFile"
+
+struct stock{
+    QString name;
+    QString tickerSymbol;
+    QString currentPrice;
+    int quantityAvailable;
+};
+
+class simulationMarket : public QThread
 {
 public:
     simulationMarket();
+    void run();
+
+
+    void generateFakeMarketFromFile(QFile inputFile);
+
+    QList<*stock> *stocks;
+
 };
 
 #endif // SIMULATIONMARKET_H
